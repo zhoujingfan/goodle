e3a03e08e3f2bc2a14a34485cebcf4cbf05ba9f4
jeanrry <2753607241@qq.com>
2019-03-18 21:48:30 +0800

    1 瞎改了些东西

--- goodle/urls.py
+++ goodle/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from app.goodle_web.views import index
+from goodle_web.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- goodle_vue/src/components/404.vue
+++ goodle_vue/src/components/404.vue
@@ -1,8 +1,19 @@
 <template>
   <div class="not-found">
-    <img src="../assets/notFound.png">
-    <h1>对不住，兄弟，这页没了</h1>
-    <h3>试试<router-link to="/Home">去首页</router-link></h3>
+    <div class="page-body">
+      <el-row :gutter="10">
+        <el-col :xs="2" :sm="8" :md="8" :lg="8" :xl="9">
+          <div class="grid-content"></div>
+        </el-col>
+        <el-col :xs="20" :sm="8" :md="8" :lg="8" :xl="6">
+          <div>
+            <img src="../assets/notFound.png">
+            <!--<h1>对不住，兄弟，这页没了</h1>-->
+            <h3>试试<router-link to="/Home">去首页</router-link></h3>
+          </div>
+        </el-col>
+      </el-row>
+    </div>
   </div>
 </template>
 
@@ -17,7 +28,13 @@ export default {
   text-align: center;
 }
 .not-found img{
-  width: 500px;
+  width: 100%;
   top: 50px;
+}
+.page-body {
+  padding-top: 60px;
+}
+.grid-content {
+  min-height: 36px;
 }
 </style>
