ae699549fd53ec313468db2e9fd78c5606c7c175
zhoujingfan <863321350@qq.com>
2019-06-10 11:32:54 +0800

    不知道更新了什么

--- gamesearch/views.py
+++ gamesearch/views.py
@@ -6,10 +6,8 @@ from elasticsearch_dsl.connections import connections
 es = Elasticsearch('152.136.129.232:9200')
 
 def search(request):
-    print("OK")
     content = {}
     if request.method == "POST":
-        print("POST")
         keyword = request.POST.get('keyword')
         # search_type = request.POST.get('search_type')
         # page_num = int(request.POST.get('page_num'))
@@ -24,26 +22,56 @@ def search(request):
             "query": {
                 "match": {
                     "game_name": keyword
+                },
+                # "match": {
+                #     "game_summary":keyword
+                # }
+            },
+            "highlight": {
+                "fields":{
+                    "game_name":{}
                 }
             }
         }
+        baseweb = request.POST.get('baseWeb')
+        print(baseweb)
+        print(len(baseweb))
+        print(keyword)  
         res = es.search(index="steamindex",doc_type="steam",body=doc)
         res2 = es.search(index='wegameindex',doc_type='wegame',body=doc)
-        total = res['hits']['total']+res2['hits']['total']
         result_list2 = res2['hits']['hits']
-        result_list = res['hits']['hits']
+        result_list1 = res['hits']['hits']
+        station = []
+        if len(result_list1)>0:
+            station.append('steam')
+        if len(result_list2)>0:
+            station.append('wegame')
+        if len(baseweb)>6:
+            total = res['hits']['total']+res2['hits']['total']
+            result_list  = result_list1
+            for item in result_list2:
+                result_list.append(item)
+        elif baseweb=="steam":
+            total = res['hits']['total']
+            result_list = result_list1
+        elif baseweb=='uplay':
+            total = res2['hits']['total']
+            result_list = result_list2
+        else:
+            content['code']=102
+            content['message']="参数错误"
+            return JsonResponse(content)
+        print(result_list1)
+        print(result_list2)
+        print(len(result_list1))
         print(len(result_list2))
-        print(len(result_list))
-        result = []
-        for item in result_list2:
-            result_list.append(item)
-        print(len(result_list))
         # for num in range(page_num*10,min(len(result_list), page_num*10+10)):
         #     result.append(result_list[num])
         # content['result'] = result
         content['result_list'] = result_list
         # content['result_list2'] = result_list2
         content['total']=total
+        content['station'] = station
         result = "this is result"
         content['code'] = 100
         content['message'] = 'success'
@@ -83,7 +111,6 @@ def fuzzy_search(request):
 
 def suggest(request):
     content = {}
-    print("ok")
     if request.method  == "POST":
         keyword = request.POST.get('keyword')
         if not keyword:
